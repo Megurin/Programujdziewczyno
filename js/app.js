5b5e9b875a313b1918b709a36068a83b087cb11e
Anna <anna_ulfik@wp.pl>
2017-10-07 16:39:17 +0200

    zadanie2

--- js/app.js
+++ js/app.js
@@ -16,4 +16,45 @@ document.addEventListener('DOMContentLoaded', function() {
       	dropdown.style.display = 'none';
       });
 
+      // zadanie 2
+      var buttons = document.querySelectorAll('.read-more');
+      console.log(buttons);
+
+      console.log(buttons[0].previousElementSibling);
+
+      buttons[0].addEventListener('click', function() {
+      console.log('działa');
+      });
+
+      buttons[0].addEventListener('click', function() {
+      console.log(this);
+  	  });
+
+  	  buttons[0].addEventListener('click', function() {
+      var textArea = this.previousElementSibling;
+      console.log(textArea);
+      });
+
+      buttons[0].addEventListener('click', function() {
+      var textArea = this.previousElementSibling;
+      textArea.style.display = 'block';
+      });
+
+      buttons[0].addEventListener('click', function() {
+      var textArea = this.previousElementSibling;
+      textArea.style.display = 'block';
+      this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+      });
+
+      buttons[0].addEventListener('click', function() {
+      	if (textArea.style.display === 'none' || textArea.style.display === '') {
+      	textArea.style.display = 'block';
+        this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+      	} 
+      	else {
+      	textArea.style.display = 'none';
+        this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+        }
+      });
+
  });
